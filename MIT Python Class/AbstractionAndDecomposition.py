f5752685ed015481fafe27201e246d9469e0f577
Manisha Roshini <manisharoshini10@gmail.com>
2026-05-04 14:07:14 +0530

    4th May 14:07 commit

--- MIT Python Class/AbstractionAndDecomposition.py
+++ MIT Python Class/AbstractionAndDecomposition.py
@@ -84,10 +84,67 @@
 # print(is_even_with_return(5))
 
 # even function without remainder 
-def is_even_without_remiander(i):
-    print("without remainder")
-    remainder = i % 2
-    has_rem = (remainder == 0)
-    print(has_rem)
+# def is_even_without_remiander(i):
+#     print("without remainder")
+#     remainder = i % 2
+#     has_rem = (remainder == 0)
+#     print(has_rem)
     
-print(is_even_without_remiander(4))
+# print(is_even_without_remiander(4))
+
+# you Try it
+
+# def add(x,y):
+#     return x + y
+
+# def mult(x,y):
+#     print(x*y)
+
+# print(add(3,4))
+# mult(3,4) # since the print statement is in the function itself 
+# print(mult(4,5))# print 20 and NONE here entire function calls become none !! so it will become print(None) --> mult(4,5) becomes None 
+# # return only has value inside the function 
+
+# Try it yourself: Triangular Number 
+
+# def is_triangular(n):
+#     total = 0
+#     for i in range(n + 1):
+#         total += i
+#         if total == n:
+#             return True
+#     return False
+                   
+#     #         print(True) ] trial and error.. 
+#     # print(False)        ]  initially used print frunction then used return function
+            
+# print(is_triangular(4))
+
+
+# Bisection search using Functions:
+def bisection_root(x):
+    epsilon = 0.01
+    low = 0
+    high = x
+    ans = (high + low)/2
+    while abs(ans**2 - x) > epsilon:
+        if ans**2 < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2
+    return ans 
+
+print(bisection_root(4))
+print(bisection_root(123))
+
+# epsilon is the level of error whuch is acceotable or else it goes to infinite loop    
+# ---------------------------------------------------------------------------------------------
+# Try it Yourself
+# calculate all the values which is close to the the n and epsilon
+# Eg:
+# 1. sqrt 99 is 9.94969401
+# 2. sqrt of 100 is 9.9998474
+# 3. sqrt of 101 is 10.0497589111
+# 4. sqrt of 102 is 10.0994567871
+# ---------------------------------------------------------------------------------------------
