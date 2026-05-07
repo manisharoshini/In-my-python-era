9a2f4cb535e2c55b879a638e3572c913b304c271
Manisha Roshini <manisharoshini10@gmail.com>
2026-05-07 20:45:48 +0530

    7th may 8:45 Tuples Introcuction

---
+++ MIT Python Class/Tuples.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------------
+# INTRODUCTION TO TUPLES
+# --------------------------------------------------------------
+
+# tuple = () # Empty Tuple
+# tuple1 = (2,)# it will consider ) as a tuple 
+# # Above code tells python that this is sequence of object but there is just one object in my sequence
+# print(type(tuple1)) # type of this is an tuple not the integer 
