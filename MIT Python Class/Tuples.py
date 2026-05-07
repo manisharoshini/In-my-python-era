71c894273dba18f579504ebb79a96217647c47f1
Manisha Roshini <manisharoshini10@gmail.com>
2026-05-07 20:54:38 +0530

    7th may 8:55 Tuples Introcuction

--- MIT Python Class/Tuples.py
+++ MIT Python Class/Tuples.py
@@ -6,3 +6,18 @@
 # tuple1 = (2,)# it will consider ) as a tuple 
 # # Above code tells python that this is sequence of object but there is just one object in my sequence
 # print(type(tuple1)) # type of this is an tuple not the integer 
+
+# # Multiple element tuples:
+tup1 = (2, "mit", 3)
+print(tup1[0])
+# [] -> is used to index in tuple 
+
+# tup2 = (1, "mit",3) + (6,7) # we use + sign to add extra elemnets to tuples
+# print(tup2) 
+
+# tup3 = tup1 + (4,5) # we can add extra elemnets and tuples to this in this way too
+# print(tup3)
+
+# tup4 = tup1 + tup2 + tup3 # IN this way we acn concatenate multipe tuples
+# print(tup4)
+
