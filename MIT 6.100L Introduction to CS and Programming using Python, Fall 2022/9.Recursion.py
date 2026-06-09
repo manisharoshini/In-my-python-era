f4335fb41476b5886f78234028e58b81d0a5a1a9
Manisha Roshini <manisharoshini10@gmail.com>
2026-06-09 14:17:31 +0530

    9th Juen 2:17 completed (Lec 15: Recursion)

--- MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/9.Recursion.py
+++ MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/9.Recursion.py
@@ -41,13 +41,43 @@ Semantically: A programming technique where a function calls itself (with differ
 
 """
 
-# -- Try it yourself: calculate n to the power p for variables n and p (2 base cases included one for zero and another for 1)
-def power_recur(n,p):
-    if p == 0:
-        return 1
-    elif p == 1:
-        return n 
-    else: 
-        return n * power_recur(n,p-1)
+# # -- Try it yourself: calculate n to the power p for variables n and p (2 base cases included one for zero and another for 1)
+# def power_recur(n,p):
+#     if p == 0:
+#         return 1
+#     elif p == 1: # not importanat this is just to show we can add 2 base functions
+#         return n 
+#     else: 
+#         return n * power_recur(n,p-1)
 
-print(power_recur(6,2))
+# print(power_recur(6,2))
+
+# --  Recurisve factorial:
+def rec_fac(n):
+    if n == 1:
+        return 1
+    else:
+        return n * rec_fac(n-1)
+    
+print(rec_fac(6))
+
+"""
+Each function call has its own environment.. they dont interfere in other env and even though they have same name (diff parameters)
+they'll considered as different function (env)
+Using same variables but they have different objects in differnt scope 
+FLow control -- when we make a function call all we know is the function that we call next and who we need to give the value back upto
+--
+Recursion is efficient from programmer POV but not from computer POV
+--
+When to use recursion?
+when we need to do repeated task for we dont know how deep we want to go - in this case recursive are used
+Since it calls itself again and again until we reach base case 
+-- 
+Case: where we use recusrion
+Eg: we can use it in file system for file search (here we dont know how deep the file is saved.. so we use recurisve)
+
+Eg: Experssions - when we create our own calculator(in code) we dont know how many operations using paranthesis. 
+Here we hv to go through each paranthesis until we reach base operations
+
+"""
+
