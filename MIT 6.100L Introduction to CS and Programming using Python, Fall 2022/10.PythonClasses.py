9a7a4c40c7d1e0ad5b0f9909f27671eb2a43bae0
Manisha Roshini <manisharoshini10@gmail.com>
2026-06-23 20:00:29 +0530

    MOdifications done

--- MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/10.PythonClasses.py
+++ MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/10.PythonClasses.py
@@ -1,1 +1,13 @@
 # # ---------------------------------------------------------------------------------------------------------------
+"""
+*** OBJECT ORIENTED PROGRAMMING ***
+
+the idea of OOP since everything in python is objects we use object oriented programming. 
+We can CREATE, MANIPULATE and even DESTROY objects in python
+
+Data Absrtaction - captures internal representations and interface for interacting with objects. 
+1. through methods and 
+2. defines behavior and hides implementation. 
+
+"""
+# refer notebook notes for more clear notes.
--- MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/9.Recursion.py
+++ MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/9.Recursion.py
@@ -3,31 +3,31 @@
 # -- Multiplication in other way to understand loops and iterations
 
 # -- Using For loop --
-# def mult(a,b):
-#     total = 0
-#     for i in range(b):
-#         total += a
-#     return total
-# print(f"The multiplication is {mult(6,7)}")
+def mult(a,b):
+    total = 0
+    for i in range(b):
+        total += a
+    return total
+print(f"The multiplication is {mult(6,7)}")
 
-# # --Using While loop -- 
-# def mult(a,b):
-#     result = 0
-#     while b > 0:
-#         result = result + a 
-#         b -= 1 # here the counter starts from zera and decreses way down to 0
-#     return result
-# print(mult(6,7))
+# --Using While loop -- 
+def mult(a,b):
+    result = 0
+    while b > 0:
+        result = result + a 
+        b -= 1 # here the counter starts from zera and decreses way down to 0
+    return result
+print(mult(6,7))
 
 # here the recursive step is a + a*(b-1) 
 
 # -- Recursive addition : Run on python tutor for clearity 
-# def mult_recur(a,b):
-#     if b == 1:
-#         return a 
-#     else:
-#         return a + mult_recur(a,b-1) # function is called inside body itself 
-# print(mult_recur(4,5))
+def mult_recur(a,b):
+    if b == 1:
+        return a 
+    else:
+        return a + mult_recur(a,b-1) # function is called inside body itself 
+print(mult_recur(4,5))
 # this will create chains of function call.. 
 # here we created loop by just calling function inside function body - this is called Recursion 
 
@@ -41,25 +41,25 @@ Semantically: A programming technique where a function calls itself (with differ
 
 """
 
-# # -- Try it yourself: calculate n to the power p for variables n and p (2 base cases included one for zero and another for 1)
-# def power_recur(n,p):
-#     if p == 0:
-#         return 1
-#     elif p == 1: # not importanat this is just to show we can add 2 base functions
-#         return n 
-#     else: 
-#         return n * power_recur(n,p-1)
+# -- Try it yourself: calculate n to the power p for variables n and p (2 base cases included one for zero and another for 1)
+def power_recur(n,p):
+    if p == 0:
+        return 1
+    elif p == 1: # not importanat this is just to show we can add 2 base functions
+        return n 
+    else: 
+        return n * power_recur(n,p-1)
 
-# print(power_recur(6,2))
+print(power_recur(6,2))
 
-# # --  Recurisve factorial:
-# def rec_fac(n):
-#     if n == 1:
-#         return 1
-#     else:
-#         return n * rec_fac(n-1)
+# --  Recurisve factorial:
+def rec_fac(n):
+    if n == 1:
+        return 1
+    else:
+        return n * rec_fac(n-1)
     
-# print(rec_fac(6))
+print(rec_fac(6))
 
 """
 Each function call has its own environment.. they dont interfere in other env and even though they have same name (diff parameters)
@@ -80,223 +80,223 @@ Eg: Experssions - when we create our own calculator(in code) we dont know how ma
 Here we hv to go through each paranthesis until we reach base operations
 
 """
-# # ==========================================================================================================================
+# ==========================================================================================================================
 
-# # -----------------------------------------------------------
-# # -- Lecture 16: Recursion on non numerics
-# # -----------------------------------------------------------
+# -----------------------------------------------------------
+# -- Lecture 16: Recursion on non numerics
+# -----------------------------------------------------------
 
-# # -- fibonacci series -- 
+# -- fibonacci series -- 
 
-# def fib(x):
-#     if x == 1 or x == 2:
-#         return 1
-#     else:
-#         return fib(x-1) + fib(x-2) # it calls itself twice 
+def fib(x):
+    if x == 1 or x == 2:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2) # it calls itself twice 
     
-# print(fib(6))
+print(fib(6))
 
 """
 Since in this we are recalculating the value again and agin we use more efficinet way.
 We have to keep track of the data and numbers.. So we use dictionaries for that to store values
 """
 
-# def fib_efficient(n,d): # memorized fibonacci series solution 
-#     if n in d:
-#         return d[n] 
-#     else:
-#         ans = fib_efficient(n-1,d) + fib_efficient(n-2,d)
-#         d[n] = ans
-#         print(d[n]) # for my convenience
-#     return ans
+def fib_efficient(n,d): # memorized fibonacci series solution 
+    if n in d:
+        return d[n] 
+    else:
+        ans = fib_efficient(n-1,d) + fib_efficient(n-2,d)
+        d[n] = ans
+        print(d[n]) # for my convenience
+    return ans
 
-# d = {1:1, 2:1}
-# print(fib_efficient(7,d))
-# print(fib_efficient(34,d)) # -- here in upper code it will make 11 million calls for functions
-# # but in efficent one there's only 65 calls made
-# # the 1st code stores nothing but it is slow.. BUt efficeint algo stores things uses memory and is fast
+d = {1:1, 2:1}
+print(fib_efficient(7,d))
+print(fib_efficient(34,d)) # -- here in upper code it will make 11 million calls for functions
+# but in efficent one there's only 65 calls made
+# the 1st code stores nothing but it is slow.. BUt efficeint algo stores things uses memory and is fast
 
-# # here it will check for repeated calculations if it found it will use that and there's no need to solve again 
-# # mo need to calculate just check the dictionary dict 
+# here it will check for repeated calculations if it found it will use that and there's no need to solve again 
+# mo need to calculate just check the dictionary dict 
 
-# # ---------------------------------------------------------------------------------------------------------------==
-# # -- Try It Yourslef: Returns all ways to make a score of x by adding 1,2 and/or 3 together. Order doesnt matter ..
+# ---------------------------------------------------------------------------------------------------------------==
+# -- Try It Yourslef: Returns all ways to make a score of x by adding 1,2 and/or 3 together. Order doesnt matter ..
 
-# def score_count(x):
-#     if x == 1:
-#         return 1
-#     elif x == 2:
-#         return 2
-#     elif x == 3:
-#         return 3
-#     else:
-#         return score_count(x-1) + score_count(x-2) + score_count(x-3)
+def score_count(x):
+    if x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    elif x == 3:
+        return 3
+    else:
+        return score_count(x-1) + score_count(x-2) + score_count(x-3)
     
-# print(score_count(10)) # try using efiicinet method 
+print(score_count(10)) # try using efiicinet method 
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # SUM OF LIST NUMBERS
+# ---------------------------------------------------------------------------------------------------------------
+# SUM OF LIST NUMBERS
 
-# # Lists can have other list inside it other lists (nested list can be at any level deep)
-# # Iteration is possible only till certain ;evel and its hard
-# # Instead we can use recursive method - no matter how deep the nested list goes.. we can work using repeated steps
+# Lists can have other list inside it other lists (nested list can be at any level deep)
+# Iteration is possible only till certain ;evel and its hard
+# Instead we can use recursive method - no matter how deep the nested list goes.. we can work using repeated steps
 
 
-# #  -- Normal Method 
-# def total_iter(L):
-#     result = 0
-#     for i in L:
-#         result += i
-#     return result
+#  -- Normal Method 
+def total_iter(L):
+    result = 0
+    for i in L:
+        result += i
+    return result
 
-# print(total_iter([10,20,30,40,50]))
+print(total_iter([10,20,30,40,50]))
 
-# # -- Recursive Method 
-# def total_recur(L):
-#     if L == 0:
-#         return 0
-#     elif len(L) == 1:
-#         return L[0]
-#     else:
-#         return L[0] + total_recur(L[1:])
+# -- Recursive Method 
+def total_recur(L):
+    if L == 0:
+        return 0
+    elif len(L) == 1:
+        return L[0]
+    else:
+        return L[0] + total_recur(L[1:])
 
-# print(total_recur([10,20,30,40,50]))
+print(total_recur([10,20,30,40,50]))
 
-# # -- Try it yourself --
-# # Modify the code we wrote to return the total length of all strings in L 
+# -- Try it yourself --
+# Modify the code we wrote to return the total length of all strings in L 
 
-# def total_len_recur(L):
-#     if len(L) == 1:
-#         return len(L[0])
-#     else:
-#         return len(L[0]) + total_len_recur(L[1:])
-# print(total_len_recur(["ab","cd","efgh"]))
-# # the normal one is more efficient as compared to this.. this has repeated fuction calls
-# # Uing build in ones are more efficient rather than this one 
+def total_len_recur(L):
+    if len(L) == 1:
+        return len(L[0])
+    else:
+        return len(L[0]) + total_len_recur(L[1:])
+print(total_len_recur(["ab","cd","efgh"]))
+# the normal one is more efficient as compared to this.. this has repeated fuction calls
+# Uing build in ones are more efficient rather than this one 
 
-# # ---------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------
 
-# # Looking for an element in the list 
-# # Base Case - len of element 1 is the same element we are looking for 
-# def in_list(L,e):
-#     print(e,L) # -- to check exactly whats going on loop 
-#     if len(L) == e:
-#         return L[0] == e
-#     else:
-#         if L[0] == e:
-#             return True
-#         return in_list(L[1:],e)
+# Looking for an element in the list 
+# Base Case - len of element 1 is the same element we are looking for 
+def in_list(L,e):
+    print(e,L) # -- to check exactly whats going on loop 
+    if len(L) == e:
+        return L[0] == e
+    else:
+        if L[0] == e:
+            return True
+        return in_list(L[1:],e)
 
-# test = [2,8,4,1]
-# print(in_list(test,1)) # -- returns True
+test = [2,8,4,1]
+print(in_list(test,1)) # -- returns True
 
-# tes = [2,1,5,8]
-# print(in_list(tes,1)) # -- returns False coz the above code is checking only the last element is the element we are looking for 
-# # it is not looking the first element or somethinggg only comparing the last element with the element we needed 
-# # extracts first element but dosent do anything 
+tes = [2,1,5,8]
+print(in_list(tes,1)) # -- returns False coz the above code is checking only the last element is the element we are looking for 
+# it is not looking the first element or somethinggg only comparing the last element with the element we needed 
+# extracts first element but dosent do anything 
 
-# # -- improved way to write it in more pythonic way:
-# def improve_inlist(L,e):
-#     if len(L) == e:
-#         return False
-#     elif L[0] == e:
-#         return True
-#     else:
-#         return improve_inlist(L[1:],e)
+# -- improved way to write it in more pythonic way:
+def improve_inlist(L,e):
+    if len(L) == e:
+        return False
+    elif L[0] == e:
+        return True
+    else:
+        return improve_inlist(L[1:],e)
 
-# test = [2,8,4,1]
-# print(improve_inlist(test,1)) # -- returns True
+test = [2,8,4,1]
+print(improve_inlist(test,1)) # -- returns True
 
-# tes = [2,1,5,8]
-# print(improve_inlist(tes,1)) # --returns True 
+tes = [2,1,5,8]
+print(improve_inlist(tes,1)) # --returns True 
 
-# # ---------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------
 
-# # -- FLATTEN a LIST with only ONE LEVEL of LIST ELEMENTS 
+# -- FLATTEN a LIST with only ONE LEVEL of LIST ELEMENTS 
 # Eg: [[1,2],[3,4],[9,8,7]] ----> [1,2,3,4,9,8,7]
 
-# def flatten(L):
-#     if len(L) == 1:
-#         return L[0] # -- [[2,3,4]]--> grab the 2,3,4 and flatten it 
-#     else: 
-#         return L[0] + flatten(L[1:]) # -- note L[start:end] slicing starts at 1 skips 0th index 
+def flatten(L):
+    if len(L) == 1:
+        return L[0] # -- [[2,3,4]]--> grab the 2,3,4 and flatten it 
+    else: 
+        return L[0] + flatten(L[1:]) # -- note L[start:end] slicing starts at 1 skips 0th index 
 
-# test = [[1,2],[3,4],[9,8,7]]
-# print(flatten(test))
+test = [[1,2],[3,4],[9,8,7]]
+print(flatten(test))
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # -- Try it yourself: L is list whose elements are lists containing ints. Return True if e is found within the list of L. else return FALSE.
+# ---------------------------------------------------------------------------------------------------------------
+# -- Try it yourself: L is list whose elements are lists containing ints. Return True if e is found within the list of L. else return FALSE.
 
-# def list_in_list(L,e):
-#     if len(L) == 1:
-#         return e in L[0]
-#     else:
-#         first = L[0]
-#         if e in first:
-#             return True
-#         return list_in_list(L[1:],e)
+def list_in_list(L,e):
+    if len(L) == 1:
+        return e in L[0]
+    else:
+        first = L[0]
+        if e in first:
+            return True
+        return list_in_list(L[1:],e)
 
-# test = [[1,2],[3,4],[5,6,7]]
-# print(list_in_list(test, 4))
+test = [[1,2],[3,4],[5,6,7]]
+print(list_in_list(test, 4))
 
-# # You keep throwing away the first box (L[1:]) until either:
+# You keep throwing away the first box (L[1:]) until either:
 
-# # You find the number → True
-# # You reach the last box and it's not there → False
+# You find the number → True
+# You reach the last box and it's not there → False
 
-# # That's the recursive pattern
+# That's the recursive pattern
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # -- Reverse the elements in the list using recursion --
-# def reverse_list(L):
-#     if len(L) == 1:
-#         return L
-#     else:
-#         return reverse_list(L[1:]) + [L[0]]
+# ---------------------------------------------------------------------------------------------------------------
+# -- Reverse the elements in the list using recursion --
+def reverse_list(L):
+    if len(L) == 1:
+        return L
+    else:
+        return reverse_list(L[1:]) + [L[0]]
     
-# list = [1,2,'abc']
-# print(reverse_list(list))
+list = [1,2,'abc']
+print(reverse_list(list))
 
-# list1 = [1,['d'],['e',['f','g']]]
-# print(reverse_list(list1))
-# # this code will just reverse the top list not list withinn list is reversed 
+list1 = [1,['d'],['e',['f','g']]]
+print(reverse_list(list1))
+# this code will just reverse the top list not list withinn list is reversed 
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # -- Reverse all the elements in the sublists too. 
+# ---------------------------------------------------------------------------------------------------------------
+# -- Reverse all the elements in the sublists too. 
 
-# def deep_rev(L):
-#     if len(L) == 1:
-#         if type(L[0]) != list:
-#             return L
-#         return [deep_rev(L[0])]
-#     else:
-#         if type(L[0]) != list:
-#             return deep_rev(L[1:]) + [L[0]]
-#         return deep_rev(L[1:]) + [deep_rev(L[0])]
+def deep_rev(L):
+    if len(L) == 1:
+        if type(L[0]) != list:
+            return L
+        return [deep_rev(L[0])]
+    else:
+        if type(L[0]) != list:
+            return deep_rev(L[1:]) + [L[0]]
+        return deep_rev(L[1:]) + [deep_rev(L[0])]
 
-# list1 = [1,['d'],['e',['f','g']]]
-# print(deep_rev(list1))
+list1 = [1,['d'],['e',['f','g']]]
+print(deep_rev(list1))
 
-# list2 = [[1,2,3],[4,5],[[9],[2,8,5]]]
-# print(deep_rev(list2))
+list2 = [[1,2,3],[4,5],[[9],[2,8,5]]]
+print(deep_rev(list2))
 
-# # -- CLEANED UP CODE:
+# -- CLEANED UP CODE:
 
-# def deep_rev_cleaned(L):
-#     if L == []:
-#         return []
-#     elif type(L[0]) != list:
-#         return deep_rev_cleaned(L[1:]) + [L[0]]
-#     else:
-#         return deep_rev_cleaned(L[1:]) + [deep_rev_cleaned(L[0])]
+def deep_rev_cleaned(L):
+    if L == []:
+        return []
+    elif type(L[0]) != list:
+        return deep_rev_cleaned(L[1:]) + [L[0]]
+    else:
+        return deep_rev_cleaned(L[1:]) + [deep_rev_cleaned(L[0])]
 
-# list1 = [1,['d'],['e',['f','g']]]
-# print(deep_rev_cleaned(list1))
+list1 = [1,['d'],['e',['f','g']]]
+print(deep_rev_cleaned(list1))
 
-# list2 = [[1,2,3],[4,5],[[9],[2,8,5]]]
-# print(deep_rev_cleaned(list2))
+list2 = [[1,2,3],[4,5],[[9],[2,8,5]]]
+print(deep_rev_cleaned(list2))
 
-# # -- same code and logic can work on tuples and strings except for one since we dont have strings within strings concept.
+# -- same code and logic can work on tuples and strings except for one since we dont have strings within strings concept.
 
 """
 tips:
