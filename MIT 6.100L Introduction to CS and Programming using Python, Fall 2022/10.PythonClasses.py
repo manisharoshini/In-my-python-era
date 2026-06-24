5cb40dbfd19ddaed02d6a096f7f6f57f82e97646
Manisha Roshini <manisharoshini10@gmail.com>
2026-06-24 19:37:54 +0530

    14th July 2026 -- Lec 17: Python classes(only theorey section)

--- MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/10.PythonClasses.py
+++ MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/10.PythonClasses.py
@@ -16,7 +16,18 @@ How to interface with lists, manipulate lists ?
 len(), max(), min(), del(), 
 append(), extend(), count(), index(), insert(), pop(), remove(), reverse(), sort()
 
-Real life Examples: -- check notes for detailed ones-- 
+Real life Examples: 
+
+1. Elevator - changes floors
+2. Employee - a person who works in a company
+3. Queue at Store - First customer to arrive is the first person to serve (FIFO)
+4. Stack of Pancakes - first pancake made is last one to be eaten (LIFO)
+
+The idea of OOP -- here we are bundelling the data and the behavior in one thing 
+They are going to be consistent in data we represent them and consistent in the way that we use them
+
+YPU WRITE CLASS BEACUSE YOU MAKE DESIGN DECISIONS. - here we decide what data represents the class and you decide 
+what behaviors represent the class.
 
 """
 # refer notebook notes for more clear notes.
