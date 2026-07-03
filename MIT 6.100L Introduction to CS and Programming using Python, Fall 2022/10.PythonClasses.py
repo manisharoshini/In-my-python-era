9345b07313e898ab81b7d005e3c1de2503f3bdd2
Manisha Roshini <manisharoshini10@gmail.com>
2026-07-03 16:57:00 +0530

    3rd July 2026 -- Lecture 18

--- MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/10.PythonClasses.py
+++ MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/10.PythonClasses.py
@@ -83,165 +83,165 @@ self - is the one whoever called the method.
 """
 # # Define a method for coordinate class and going to calculate the distance
 
-# class Cordinate:
-#     def __init__(self,xval,yval):
-#         self.x = xval
-#         self.y = yval
-#     def distance(self,other): # works only with the class type cordinate we use 'self' here
-#         # Returns eucdian distance between two coord objects 
-#         x_diff_sq = (self.x - other.x) ** 2
-#         y_diff_sq = (self.y - other.y) ** 2
-#         return (x_diff_sq+y_diff_sq) ** 0.5
+class Cordinate:
+    def __init__(self,xval,yval):
+        self.x = xval
+        self.y = yval
+    def distance(self,other): # works only with the class type cordinate we use 'self' here
+        # Returns eucdian distance between two coord objects 
+        x_diff_sq = (self.x - other.x) ** 2
+        y_diff_sq = (self.y - other.y) ** 2
+        return (x_diff_sq+y_diff_sq) ** 0.5
     
-# c = Cordinate(3,4)
-# orig = Cordinate(0,0)
+c = Cordinate(3,4)
+orig = Cordinate(0,0)
 
-# print(Cordinate.distance(c,orig))
-# print(c.distance(orig)) # similar to mylist.append(5)
+print(Cordinate.distance(c,orig))
+print(c.distance(orig)) # similar to mylist.append(5)
 
 # ===============================================================================================================
 # Lec 18: More Python Classes Methods 
 # ===============================================================================================================
 
-# class Cordinate:
-#     def __init__(self,xval,yval):
-#         self.x = xval
-#         self.y = yval
-#     def distance(self,other): # works only with the class type cordinate we use 'self' here
-#         # Returns eucdian distance between two coord objects 
-#         x_diff_sq = (self.x - other.x) ** 2
-#         y_diff_sq = (self.y - other.y) ** 2
-#         return (x_diff_sq+y_diff_sq) ** 0.5
-#     def to_origin(self):
-#         self.x = 0
-#         self.y = 0
-# c = Cordinate(3,4)
-# origin = Cordinate(0,0)
+class Cordinate:
+    def __init__(self,xval,yval):
+        self.x = xval
+        self.y = yval
+    def distance(self,other): # works only with the class type cordinate we use 'self' here
+        # Returns eucdian distance between two coord objects 
+        x_diff_sq = (self.x - other.x) ** 2
+        y_diff_sq = (self.y - other.y) ** 2
+        return (x_diff_sq+y_diff_sq) ** 0.5
+    def to_origin(self):
+        self.x = 0
+        self.y = 0
+c = Cordinate(3,4)
+origin = Cordinate(0,0)
 
-# print(f"c's x-cord is {c.x} and origin's x-cord is {origin.x}  ")
-# print(c.distance(origin))
+print(f"c's x-cord is {c.x} and origin's x-cord is {origin.x}  ")
+print(c.distance(origin))
 
-# c.to_origin()
-# print(c.x,c.y)
+c.to_origin()
+print(c.x,c.y)
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # USING CLASSES TO BUILD OTHER CLASSES 
+# ---------------------------------------------------------------------------------------------------------------
+# USING CLASSES TO BUILD OTHER CLASSES 
 
-# # Example: use coordinates to build cirle 
-# # Hint: here the centre is a cordinate so we here make use of cordinate class to make a complex class 
+# Example: use coordinates to build cirle 
+# Hint: here the centre is a cordinate so we here make use of cordinate class to make a complex class 
 
-# # -- You try it: 
-# # Add a code to init method to check that type of center is obj and type of radius is int if not then raise ValueError 
+# -- You try it: 
+# Add a code to init method to check that type of center is obj and type of radius is int if not then raise ValueError 
 
-# class Cordinate:
-#     def __init__(self,xval,yval):
-#         self.x = xval
-#         self.y = yval
-#     def distance(self,other): # works only with the class type cordinate we use 'self' here
-#         # Returns eucdian distance between two coord objects 
-#         x_diff_sq = (self.x - other.x) ** 2
-#         y_diff_sq = (self.y - other.y) ** 2
-#         return (x_diff_sq+y_diff_sq) ** 0.5
-#     def to_origin(self):
-#         self.x = 0
-#         self.y = 0
+class Cordinate:
+    def __init__(self,xval,yval):
+        self.x = xval
+        self.y = yval
+    def distance(self,other): # works only with the class type cordinate we use 'self' here
+        # Returns eucdian distance between two coord objects 
+        x_diff_sq = (self.x - other.x) ** 2
+        y_diff_sq = (self.y - other.y) ** 2
+        return (x_diff_sq+y_diff_sq) ** 0.5
+    def to_origin(self):
+        self.x = 0
+        self.y = 0
 
-# class Circle(object):
-#     def __init__(self, center, radius):
-#         if type(center) != Cordinate or type(radius) != int:
-#             raise ValueError
-#         self.center = center
-#         self.radius = radius
+class Circle(object):
+    def __init__(self, center, radius):
+        if type(center) != Cordinate or type(radius) != int:
+            raise ValueError
+        self.center = center
+        self.radius = radius
     
-# center = Cordinate(2,2)
-# my_circle = Circle(center,2)
+center = Cordinate(2,2)
+my_circle = Circle(center,2)
 
-# my_circle = Circle(2,2)
-# my_circle = Circle(center, 'two')
+my_circle = Circle(2,2)
+my_circle = Circle(center, 'two')
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # Circle Class: defination and instances 
+# ---------------------------------------------------------------------------------------------------------------
+# Circle Class: defination and instances 
 
-# # This will calculate the distance of point from center of circle, 
-# # if its greater than the radius -- the point is outside the circle else its inside the circle
+# This will calculate the distance of point from center of circle, 
+# if its greater than the radius -- the point is outside the circle else its inside the circle
 
-# class Cordinate:
-#     def __init__(self,xval,yval):
-#         self.x = xval
-#         self.y = yval
-#     def distance(self,other): # works only with the class type cordinate we use 'self' here
-#         # Returns eucdian distance between two coord objects 
-#         x_diff_sq = (self.x - other.x) ** 2
-#         y_diff_sq = (self.y - other.y) ** 2
-#         return (x_diff_sq+y_diff_sq) ** 0.5
-#     def to_origin(self):
-#         self.x = 0
-#         self.y = 0
+class Cordinate:
+    def __init__(self,xval,yval):
+        self.x = xval
+        self.y = yval
+    def distance(self,other): # works only with the class type cordinate we use 'self' here
+        # Returns eucdian distance between two coord objects 
+        x_diff_sq = (self.x - other.x) ** 2
+        y_diff_sq = (self.y - other.y) ** 2
+        return (x_diff_sq+y_diff_sq) ** 0.5
+    def to_origin(self):
+        self.x = 0
+        self.y = 0
 
-# class Circle(object):
-#     def __init__(self, center, radius):
-#         # if type(center) != Cordinate or type(radius) != int:
-#         #     raise ValueError
-#         self.center = center # here its a cordinate object 
-#         self.radius = radius # here its an int 
-#     def is_inside(self,point):
-#         return point.distance(self.center) < self.radius
-#     def is_inside2(self,point):
-#         return (self.center).distance(point) <self.radius # -- here this means it doesnt really care in which cordinate it runs on !!
+class Circle(object):
+    def __init__(self, center, radius):
+        # if type(center) != Cordinate or type(radius) != int:
+        #     raise ValueError
+        self.center = center # here its a cordinate object 
+        self.radius = radius # here its an int 
+    def is_inside(self,point):
+        return point.distance(self.center) < self.radius
+    def is_inside2(self,point):
+        return (self.center).distance(point) <self.radius # -- here this means it doesnt really care in which cordinate it runs on !!
     
-# center = Cordinate(2,2)
-# my_circle = Circle(center,2)
+center = Cordinate(2,2)
+my_circle = Circle(center,2)
 
-# p = Cordinate(1,1)
-# print(my_circle.is_inside(p))
+p = Cordinate(1,1)
+print(my_circle.is_inside(p))
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # Create a new type to represent fraction:
-# # Internal representation of two integers Numerator and Denominator 
-# # Methods to interact with Fractions are -- Add, Subtract and invert 
+# ---------------------------------------------------------------------------------------------------------------
+# Create a new type to represent fraction:
+# Internal representation of two integers Numerator and Denominator 
+# Methods to interact with Fractions are -- Add, Subtract and invert 
 
-# class SimpleFraction(object):
-#     def __init__(self,n,d):
-#         self.num = n
-#         self.denom = d
+class SimpleFraction(object):
+    def __init__(self,n,d):
+        self.num = n
+        self.denom = d
 
-#     def multiply(self,oth):
-#         top = self.num * oth.num
-#         bottom = self.denom*oth.denom
-#         return top/bottom
+    def multiply(self,oth):
+        top = self.num * oth.num
+        bottom = self.denom*oth.denom
+        return top/bottom
     
-#     def addition(self,other):
-#         top = (self.num*other.denom) + (self.denom*other.num)
-#         bottom = self.denom*other.denom
-#         return top/bottom
+    def addition(self,other):
+        top = (self.num*other.denom) + (self.denom*other.num)
+        bottom = self.denom*other.denom
+        return top/bottom
     
-#     def get_inverse(self):
-#         return 1/(self.num/self.denom)
+    def get_inverse(self):
+        return 1/(self.num/self.denom)
     
-#     def invert(self):
-#         # temp = self.num
-#         # self.num = self.denom
-#         # self.denom = temp
-#         # other way:
-#         (self.num,self.denom) = (self.denom,self.num)
-#         pass
+    def invert(self):
+        # temp = self.num
+        # self.num = self.denom
+        # self.denom = temp
+        # other way:
+        (self.num,self.denom) = (self.denom,self.num)
+        pass
 
-# f1 = SimpleFraction(3,4)
-# f2 = SimpleFraction(1,4)
+f1 = SimpleFraction(3,4)
+f2 = SimpleFraction(1,4)
 
-# print(f"The invert of f1 and f2 is {f1.invert()} and {f2.invert()}") # The invert of f1 and f2 is 0.75 and 0.25
-# print(f"The inverse value of f1 and f2 is {f1.get_inverse()} and {f2.get_inverse()}") # The inverse value of f1 and f2 is 1.3333333333333333 and 4.0
+print(f"The invert of f1 and f2 is {f1.invert()} and {f2.invert()}") # The invert of f1 and f2 is 0.75 and 0.25
+print(f"The inverse value of f1 and f2 is {f1.get_inverse()} and {f2.get_inverse()}") # The inverse value of f1 and f2 is 1.3333333333333333 and 4.0
 
-# print("----Perfrom Invert function: -----")
-# print(f"Initially num = {f1.num} and denom = {f1.denom}")
-# f1.invert()
-# print(f"After Inverting Num and denom, our new num = {f1.num} and new denom = {f1.denom}")
+print("----Perfrom Invert function: -----")
+print(f"Initially num = {f1.num} and denom = {f1.denom}")
+f1.invert()
+print(f"After Inverting Num and denom, our new num = {f1.num} and new denom = {f1.denom}")
 
-# print(f"the numerator and denominator of F1 fraction is {f1.num} and {f1.denom}") # the numerator and denominator of F1 fraction is 3 and 4
-# print(f"the numerator and denominator of F1 fraction is {f2.num} and {f2.denom}") # the numerator and denominator of F1 fraction is 1 and 4
+print(f"the numerator and denominator of F1 fraction is {f1.num} and {f1.denom}") # the numerator and denominator of F1 fraction is 3 and 4
+print(f"the numerator and denominator of F1 fraction is {f2.num} and {f2.denom}") # the numerator and denominator of F1 fraction is 1 and 4
 
-# print(f"Addition of f1 and f2 = {f1.addition(f2)}") # Addition of f1 and f2 = 1.0
-# print(f"Multiplication of f1 and f2 = {f1.multiply(f2)}") # Multiplication of f1 and f2 = 0.1875
+print(f"Addition of f1 and f2 = {f1.addition(f2)}") # Addition of f1 and f2 = 1.0
+print(f"Multiplication of f1 and f2 = {f1.multiply(f2)}") # Multiplication of f1 and f2 = 0.1875
 
 """
 dunder methods:
@@ -266,31 +266,106 @@ Python secretly does 10.__add__(20) similary for all the shorthand functions
 
 """
 
-# # ---------------------------------------------------------------------------------------------------------------
-# # -- Defining our own print methods 
-# class Cordinate(object):
-#     def __init__(self,xval,yval):
-#         self.x = xval
-#         self.y = yval
-#     def distance(self,other):
-#         x_diff_sq = (self.x - other.x)**2
-#         y_diff_sq = (self.y - other.y)**2
-#         return (x_diff_sq + y_diff_sq)*0.5
-#     def __str__(self): # __str__ is a name of special method 
-#         return "<"+str(self.x)+","+str(self.y)+">"
+# ---------------------------------------------------------------------------------------------------------------
+# -- Defining our own print methods 
+class Cordinate(object):
+    def __init__(self,xval,yval):
+        self.x = xval
+        self.y = yval
+    def distance(self,other):
+        x_diff_sq = (self.x - other.x)**2
+        y_diff_sq = (self.y - other.y)**2
+        return (x_diff_sq + y_diff_sq)*0.5
+    def __str__(self): # __str__ is a name of special method 
+        return "<"+str(self.x)+","+str(self.y)+">"
     
-# cor = Cordinate(3,4)
-# origin = Cordinate(2,2)
-# print(cor.distance(origin))
+cor = Cordinate(3,4)
+origin = Cordinate(2,2)
+print(cor.distance(origin))
 
-# # -- check whether __str__ is working or not 
-# print(f"This is the way that we wanted python to code the string (manipulating __str__): {cor}")
-# print(origin)
-# print(f"Here the type of object cor is {type(cor)}, ie the name of the class we have created in")
+# -- check whether __str__ is working or not 
+print(f"This is the way that we wanted python to code the string (manipulating __str__): {cor}")
+print(origin)
+print(f"Here the type of object cor is {type(cor)}, ie the name of the class we have created in")
 
-# print(f"The type of cordinate class is {type(Cordinate)},ie we are defining a new type in the python thats y its type is 'type' ")
+print(f"The type of cordinate class is {type(Cordinate)},ie we are defining a new type in the python thats y its type is 'type' ")
 
-# print(isinstance(cor,Cordinate)) # another way to check the type of the class 
+print(isinstance(cor,Cordinate)) # another way to check the type of the class 
+
+# ---------------------------------------------------------------------------------------------------------------
+# Example: Fractions with Dunder methods (To make better fractions method)
+
+# -- Step1: Create and print Instances
+class Fraction(object):
+    def __init__(self,n,d):
+        self.num = n
+        self.deno = d
+    def __str__(self):
+        if self.deno == 1:
+            return str(self.num)
+        return str(self.num) + "/" + str(self.deno)
+# -- Step2: Implementation of +,-,*,/ float()
+
+# ----- 1. Multiplication
+    def __mul__(self,other):
+        top = self.num * other.num
+        bottom = self.deno * other.deno
+        return Fraction(top,bottom)
+# ----- 2. Addition
+    def __add__(self, other):
+        top = (self.deno * other.num) + (self.num * other.deno)
+        bottom = self.deno * other.deno
+        return Fraction(top,bottom)
+# ----- 3. Division
+    def __truediv__(self, other):
+        # -- a/b divided by c/d means a/b * d/c
+        top = self.num * other.deno
+        bottom = self.deno * other.num
+        return Fraction(top,bottom)
+# ----- 4. cast Fraction object to float to get the value of like 4/2 = 2.0 
+    def __float__(self):
+        return self.num/self.deno
+    
+# -- Step 3: reducing the Fraction like 2/12 or kindoff 
+    def reduce(self):
+        def gcd(n,d):
+            while d != 0:
+                (d,n) = (n%d,d)
+            return n 
+        if self.deno == 0:
+            return None
+        elif self.deno == 1:
+            # return self.num # -- if this loop becomes TRUE the output will of datatype 'int' instead of 'fraction'
+            # -- we cant do 4 *(1/2) since 4 is int and 1/2 is a fraction so for that we can do
+            return Fraction(self.num,1)
+        else:
+            greatest_common_factor = gcd(self.num,self.deno)
+            top = int(self.num/greatest_common_factor)
+            bottom = int(self.deno/greatest_common_factor)
+            return Fraction(top,bottom)
+
+# -- Check the step1 is working or not 
+f1 = Fraction(4,1)
+f2 = Fraction(1,4)
+f3 = Fraction(5,1)
+
+print(f"Reduce function usage: {f1} = {f1.reduce()}")
+
+print(f"Representation of fraction f1,f2,f3 is {f1},{f2},{f3}")
+
+# -- Checking step2 (__mul__)
+print(f"Multiplication of {f1} and {f2} is {f1*f2}")
+print(f"Multiplication of {f1} and {f2} is {float(f1*f2)}")
+
+# -- Checking Step2 (__Add__)
+print(f"Addition of {f1} and {f2} is {f1+f2}")
+print(f"Addition of {f1} and {f2} is {float(f1+f2)}")
+
+# -- Checking Step2(__truediv__)
+print(f"Division of {f1} and {f2} is {f1/f2}")
+print(f"Division of {f1} and {f2} is {float(f1/f2)}")
+
 
 # # ---------------------------------------------------------------------------------------------------------------
-# # Example: Fractions with Dunder methods (To make better fractions method)
+
+
