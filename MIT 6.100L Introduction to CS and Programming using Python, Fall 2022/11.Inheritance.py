fd68ad951dc27c9690610667dc9480bf5d547b29
Manisha Roshini <manisharoshini10@gmail.com>
2026-07-07 15:20:33 +0530

    7th July 2026 - Inheritance (getters and setters)

--- MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/11.Inheritance.py
+++ MIT 6.100L Introduction to CS and Programming using Python, Fall 2022/11.Inheritance.py
@@ -27,14 +27,57 @@ class Animal(object):
         self.name = newname
 
 # Testing the objects that we create: 
-myanimal = Animal(3)
-print(myanimal)
 
-# -- If we do: print(myanimal.age) and print(myanimal.get_age()) both will give me same output 
-print(myanimal.age)
-print(myanimal.get_age())
 
-# -- setname: we have set the name using setter function and get tha using getter functions 
-myanimal.set_name("Paul")
-print(myanimal.name)
-print(myanimal.get_name())
+# myanimal = Animal(3)
+# print(myanimal)
+
+# # -- If we do: print(myanimal.age) and print(myanimal.get_age()) both will give me same output 
+# print(myanimal.age)
+# print(myanimal.get_age())
+
+# # -- setname: we have set the name using setter function and get tha using getter functions 
+# myanimal.set_name("Paul")
+# print(myanimal.name)
+# print(myanimal.get_name())
+# print(myanimal) # -- this will print all the values like Age and Name 
+
+# # -- if we set name as an empty ones like
+# myanimal.set_name()
+# print(myanimal) # -- Animal::3 --> is the output coz we havent gave any name here 
+
+
+"""
+why do we use getters and setters? for Abstraction like --
+if someone changed 
+def __init__(self,age):
+        self.age = age to self.years = age 
+        self.name = None 
+AND
+def get_age(self):
+        return self.age to self.years
+    def get_name(self):
+        return self.name
+
+This internal changes are hidden from the outside user... so for that reason.. 
+a.age --> access directly to attributes(variables) means it touches internal data directly
+a.get_age()--> access only methods of the class 
+
+-- for this reason we use getter and setter rather than accessing attributes directly -- 
+
+CHATGPT Explanation:
+
+So... why didn't they just write
+myanimal.age = 10
+Because this example is teaching encapsulation.
+OOP has four major ideas:
+
+Objects
+Classes
+Inheritance
+Encapsulation ← This is where getters and setters come in.
+
+Encapsulation means:
+Don't let everyone directly touch your object's internal data. Instead, provide controlled methods to read or modify it.
+
+"""
